8d95d8ebf1f3376594346b6b1686da866d6b7d8a
RinatValiullov <4rontender@gmail.com>
2018-07-16 23:34:43 +0500

    edited math_functions.py file

---
+++ DerekBanas_tut/math_functions.py
@@ -0,0 +1,38 @@
+import math
+
+print("ceil(2.3) = ", math.ceil(2.3))
+print("floor(2.3) = ", math.floor(2.3))
+print("fabs(-2.3) = ", math.fabs(-2.3))
+
+print("factorial(8) = ", math.factorial(8))
+
+print("fmod(8,3) = ", math.fmod(8, 3))
+
+print("trunc(8.3) = ", math.trunc(8.3))
+
+print("pow(3,4) = ", math.pow(3, 4))
+
+print("sqrt(25) = ", math.sqrt(25))
+
+print("math.e = ", math.e)
+print("math.pi = ", math.pi)
+
+print("sin(90) = ", math.sin(90))
+print("cos(90) = ", math.cos(90))
+print("tan(90) = ", math.tan(90))
+print("atan(90) = ", math.atan(90))
+print("atan2(90, 2) = ", math.atan2(90, 2))
+
+print("exp(6) = ", math.exp(6))
+
+print("log(20) = ", math.log(20))
+
+print("log(27, 3) = ", math.log(27, 3))
+
+print("log10(10000) = ", math.log10(10000))
+
+print("degrees(1.5998) = ", math.degrees(1.5998))
+print("radians(60) = ", math.radians(60))
+
+
+

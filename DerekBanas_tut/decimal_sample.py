39d7f3383717bd8a14b3ef2481e2edbfcc2d4e03
RinatValiullov <4rontender@gmail.com>
2018-07-17 01:52:04 +0500

    added decimal_sample.py file

---
+++ DerekBanas_tut/decimal_sample.py
@@ -0,0 +1,11 @@
+from decimal import Decimal as dec
+
+sum = dec(0)
+sum += dec('0.1')
+sum += dec('0.1')
+sum += dec('0.1')
+sum -= dec('0.3')
+
+print("sum = ", sum)
+
+print(".1 + .1 + .1 - .3 = ", .1 + .1 + .1 - .3)
